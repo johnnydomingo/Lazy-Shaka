4dbfc5a7fb5a76e07893162cfdb929aae6e81b50
Johnny Domingo <johnnydomingo@Johnnys-MBP.lan>
2021-10-04 12:03:09 -0400

    commit

--- script.js
+++ script.js
@@ -8,12 +8,32 @@ fetch(BASE_URL)
   })
   .then((resJSON) => {
     console.log(resJSON)
+  })
+  .catch((err) => {
+    console.error(`ERROR: ${err}`)
   });
 
   /*
   Data to extract
-
+  .surf
+  .temp
+  .weather
+  .wind
   */
+
+const showBeachData = (beachData) => {
+  console.log(beachData);
+
+  const beachDataDiv = document.querySelector('#beach-data');
+  beachDataDiv.innerHTML = '';
+
+  const surfTag = document.createElement('p');
+  const tempTag = document.createElement('p');
+  const weatherTag = document.createElement('p');
+  const windTag = document.createElement('p');
+
+  surfTag.innerText = beachData[0].surf;
+}
 const searchButton = document.querySelector('#search');
 searchButton.addEventListener('click', (ev) => {
   ev.preventDefault();
